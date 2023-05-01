7e7be0ad6ed99cc52c95a4025baea2d2c4408a5a
Denis Nuriakhmetov <de.nuriakhmetov@hh.ru>
2023-05-01 13:43:22 +0300

    init spring and update gitignore

---
+++ src/main/java/shorter/OtusFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package shorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtusFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shorter/OtusFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package shorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
